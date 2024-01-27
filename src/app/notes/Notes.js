67a6a837a0a4323ff0b11c6f1c078c551f69b865
Alike73 <alimzhanisla@gmail.com>
2024-01-26 20:06:37 -0500

    modified: Notes.js

--- src/app/notes/Notes.js
+++ src/app/notes/Notes.js
@@ -11,19 +11,7 @@ const Notes = () => {
             <div className="modal-dialog modal-fullscreen modal-dialog-scrollable">
                 <div className="modal-content">
                     <div className="modal-header px-3 py-1">
-                        {/* <div>
-                            <h3 className="modal-title fs-5" id="notesLabel_1">
-                                <i className="bi bi-arrow-repeat me-2" />
-                                In progress: 15
-                            </h3>
-                            <h3 className="modal-title fs-5" id="notesLabel_2">
-                                <i className="bi bi-check-circle me-2" />
-                                Done: 7
-                            </h3>
-                        </div> */}
-                        {/* ---------------------- */}
                         <NotesNavbar />
-                        {/* ---------------------- */}
                         {/* <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close" /> */}
                         <i className="bi bi-x-square fs-1" data-bs-dismiss="modal" />
                     </div>
